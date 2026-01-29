dd1e3e1085c5e474522d2cbdfb233def8d0607c0
cjonas1999 <cjonas1999@gmail.com>
2026-01-28 18:55:57 -0600

    add way to manage config file and toggle outline mod

---
+++ MagmaMod/ConfigManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System;
+using UnityEngine.Analytics;
+using HutongGames.PlayMaker.Actions;
+using Modding;
+using UnityEngine;
+using System;
+using System.IO;
+
+namespace MagmaMod {
+    public class ConfigManager: MonoBehaviour {
+
+        public KeyCode toggle_key = KeyCode.None;
+        public bool is_enabled = false;
+
+        public static string config_path;
+
+        public ConfigManager() {
+            config_path = Application.persistentDataPath + "/MagmaMod/";
+            Directory.CreateDirectory(config_path);
+
+            string config_file = config_path + "/config.txt";
+            // https://docs.unity3d.com/6000.3/Documentation/ScriptReference/KeyCode.html
+
+            if (File.Exists(config_file)) {
+                using (TextReader reader = File.OpenText(config_file)) {
+                    toggle_key = (KeyCode)Enum.Parse(typeof(KeyCode), reader.ReadLine());
+                }
+            }
+            else {
+                toggle_key = KeyCode.F9;
+                File.WriteAllText(config_file, toggle_key.ToString());
+            }
+
+            Modding.Logger.Log($"MagmaMod toggle key set to : {toggle_key}");
+        }
+
+        public void Update() {
+            if (Input.GetKeyDown(toggle_key)) {
+                is_enabled = !is_enabled;
+                Modding.Logger.Log($"MagmaMod toggled: {is_enabled}");
+            }
+        }
+    }
+}
--- MagmaMod/MagmaMod.cs
+++ MagmaMod/MagmaMod.cs
@@ -1,35 +1,35 @@
 ﻿using HutongGames.PlayMaker.Actions;
 using Modding;
 using UnityEngine;
+using System;
+using System.IO;
 
-namespace MagmaMod
-{
-    public class MagmaMod : Mod
-    {
+namespace MagmaMod {
+    public class MagmaMod : Mod {
         public GameObject gameObject { get; private set; }
         public Circle c { get; private set; }
+        public ConfigManager manager { get; set; }
 
-        public override void Initialize()
-        {
+
+        public override void Initialize() {
+            gameObject = new GameObject();
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+
+            manager = gameObject.AddComponent<ConfigManager>();
+
             ModHooks.Instance.SceneChanged += CreateCircle;
         }
 
-        private void CreateCircle(string scene_name)
-        {
 
-            if (scene_name == "Mines_05")
-            {
-                gameObject = new GameObject();
+        private void CreateCircle(string scene_name) {
+            if (this.manager.is_enabled && scene_name == "Mines_05") {
                 c = gameObject.AddComponent<Circle>();
             }
-            else
-            {
-                Object.Destroy(c);
-                Object.Destroy(gameObject);
+            else {
+                UnityEngine.Object.Destroy(c);
             }
-
         }
 
-        public override string GetVersion() => "1.0";
+        public override string GetVersion() => "1.1";
     }
 }
